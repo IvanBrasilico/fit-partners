e78e72eb81da2f96c0a08ba0a274f0127876dd71
brasilico.ivan@gmail.com <Iv@n1234>
2019-03-19 18:07:27 -0300

    Testes passsando - primeira implementação das classes

--- app/models/club.py
+++ app/models/club.py
@@ -1,18 +1,61 @@
+from collections import defaultdict
+
+
+class WeekDay:
+    Monday = 0
+    Tuesday = 1
+    Wednesday = 2
+    Thursday = 3
+    Friday = 4
+    Saturday = 5
+    Sunday = 6
+
+
+class ClassSchedule:
+    def __init__(self, class_id,
+                 day: WeekDay, hour: int,
+                 minute: int = 0, duration: int = 30):
+        self.class_id = class_id
+        self.day = day
+        self.hour = hour
+        self.minute = minute
+        self.duration = duration
+
+
 class Class:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
+        self.schedules = defaultdict(list)
+        self.instructors = []
+
+    def add_class_schedule(self, day: WeekDay,
+                           class_schedule: ClassSchedule):
+        self.schedules[day].append(class_schedule)
+
+    def add_schedule(self, day: WeekDay, hour: int,
+                     minute: int = 0, duration: int = 30):
+        self.add_class_schedule(day, ClassSchedule(self, day, hour, minute, duration))
+
+    def get_schedules(self, weekday):
+        return self.schedules[weekday]
+
+    def add_instructor(self, instructor):
+        self.instructors.append(instructor)
+
+    def get_instructors(self):
+        return self.instructors
 
 
 class Club:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
 
 
 class Instructor:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
 
 
 class Member:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
--- app/tests/test_model.py
+++ app/tests/test_model.py
@@ -2,7 +2,8 @@
 """
 import unittest
 
-from app.models.club import Class, Club, Instructor, Member
+from app.models.club import Class, ClassSchedule, Club, Instructor, \
+    Member, WeekDay
 
 
 class ClubTestCase(unittest.TestCase):
@@ -28,3 +29,17 @@ class ClubTestCase(unittest.TestCase):
     def test_member(self):
         self.member = Member('Kirk')
         assert self.member.name == 'Kirk'
+
+    def test_schedule(self):
+        self.test_class()
+        self.aclass.add_schedule(day=WeekDay.Monday, hour=17)
+        schedules: ClassSchedule = self.aclass.get_schedules(WeekDay.Monday)
+        assert schedules[0].hour == 17
+        assert schedules[0].minute == 0
+        assert schedules[0].duration == 30
+
+    def test_instructor_classes(self):
+        self.test_class()
+        self.test_instructor()
+        self.aclass.add_instructor(self.instructor)
+        assert self.instructor in self.aclass.get_instructors()
